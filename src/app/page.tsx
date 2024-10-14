8b83bc98f3b6e3d127bf860e4566c6666257ac64
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2024-10-14 17:48:45 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  console.log("test");
+  console.log('test')
+  console.log('test2');
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -15,7 +16,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
@@ -94,9 +95,8 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org â†’
-        </a>
+          Go to nextjs.org â†        </a>
       </footer>
     </div>
-  );
+  )
 }
